055409e30ef744e90d0bf765760f2ddf8c333a25
tom_glb <goodMorning_glb@guigu.com>
2020-03-21 20:51:22 +0800

    Initial commit

---
+++ DynamicWebProject01/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Sun
+  Date: 2020/3/21
+  Time: 19:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>test</title>
+  </head>
+  <body>
+  <h1 style="color: #ff3b7c">hello world!</h1>
+  </body>
+</html>
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.atguigu.java;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author sunhongguang
+ * @create 2020-02-21-13:51
+ */
+public class HelloWorld {
+
+    /*
+        main方法
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        int I1 = 1 + 3;
+        I1 = I1 +1;
+        System.out.println("hello world");
+
+        StringBuffer stringBuffer = new StringBuffer();
+        List<String> list = new ArrayList<>();
+        Set set = new TreeSet();
+        System.out.println();
+        set.add("a");
+        "abac".toString();
+
+        ArrayList arrayList = new ArrayList();
+
+    }
+}
